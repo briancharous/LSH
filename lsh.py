fb42b0f4126b4a1ca84ba1aab6e7831d75db543b
Brian Charous <brian.charous@gmail.com>
2015-01-20 17:47:00 -0600

    read/write matrix to file; refactor input arguments; pretty progress messages

--- lsh.py
+++ lsh.py
@@ -16,6 +16,8 @@ To run: argument 1 is the filename containing the word counts
 from __future__ import division
 import sys
 import random
+import marshal
+import argparse
 
 def get_data(filename, lines):
     """ read in data from filename """
@@ -63,7 +65,7 @@ def signature_matrix(functions, docs):
     """ Return a signature matrix with given hash function lists and attribute lists
     """
     rows = len(functions)
-    columns =  len (docs)
+    columns = len(docs)
     matrix = init_sig_matrix(rows, columns)     #initialiing the matrix
     for i, function in enumerate(functions): 
         for doc in docs:
@@ -73,6 +75,13 @@ def signature_matrix(functions, docs):
                 if cur_value < matrix[i][doc-1]:    # replace if current value is smaller than the stored value
                     matrix[i][doc-1] = cur_value
 
+        # update user on progress
+        percent = (i/rows)*100
+        sys.stdout.write('\r[{0}] {1}%'.format('#'*int(percent/5), percent))
+        sys.stdout.flush()
+
+    sys.stdout.write('\r[{0}] {1}%\n'.format('#'*int(100/5), 100))
+    sys.stdout.flush()
     return matrix
 
 def jarccard_probability(matrix, doc_id_1, doc_id_2, n):
@@ -86,24 +95,69 @@ def jarccard_probability(matrix, doc_id_1, doc_id_2, n):
             same_count += 1
     return same_count/n
 
-def main():
-    if len(sys.argv) != 6:
-        print "Usage: pypy {0} input_file num_lines num_hash_functions doc_1_id doc_id_2".format(sys.argv[0])
-        exit(0)
+def save_sig_matrix(matrix, filename):
+    """ dump signature matrix to file so it doesn't constantly have to be recreated """
+    with open(filename, 'w') as f:
+        marshal.dump(matrix, f, 2)
 
+def load_sig_matrix(filename):
+    """ load signature matrix from file """
+    with open(filename, 'r') as f:
+        matrix = marshal.load(f)
+        return matrix
+    return None
+
+def main():
+    # if len(sys.argv) != 6:
+    #     print "Usage: pypy {0} input_file num_lines num_hash_functions doc_1_id doc_id_2".format(sys.argv[0])
+    #     exit(0)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--words', required=True, help='File containing document word counts')
+    parser.add_argument('-l', '--lines', required=False, help='Number of lines to read from the words file')
+    parser.add_argument('-n', '--number_of_hashes', required=True, help='Number of has functions to include in the signature matrix')
+    parser.add_argument('-d1', '--document1', required=True, help='First document ID to compare')
+    parser.add_argument('-d2', '--document2', required=True, help='Second document ID to compare')
+    parser.add_argument('-o', '--matrix_output', required=False, help='Filename to dump matrix to')
+    parser.add_argument('-m', '--matrix_file', required=False, help='Filename of saved signature matrix')
+    args = parser.parse_args()
+
+    # read in words
+    sys.stdout.write("Reading in words...")
+    sys.stdout.flush()
     num_docs = None
-    if sys.argv[2] == 'all':
+    if args.lines in (None, 'all'):
         num_docs = sys.maxint
     else:
-        num_docs = int(sys.argv[2])
-    docs, all_words = get_data(sys.argv[1], num_docs)
-    random.seed(27945)
-    num_hash_funcs = int(sys.argv[3])
-    funcs = [gen_hash_func(len(all_words)) for i in xrange(num_hash_funcs)]
-    m = signature_matrix(funcs, docs)
-    doc_id_1 = int(sys.argv[4])
-    doc_id_2 = int(sys.argv[5])
-    jaccard_estimate = jarccard_probability(m, doc_id_1, doc_id_2, num_hash_funcs)
+        num_docs = int(args.lines)
+    docs, all_words = get_data(args.words, num_docs)
+    print(" done!")
+
+    matrix = None
+    num_hash_funcs = int(args.number_of_hashes)
+    if args.matrix_file is not None:
+        # read in sig matrix from file
+        sys.stdout.write("Reading matrix from file...")
+        sys.stdout.flush()
+        with open(args.matrix_file, 'r') as f:
+            matrix = marshal.load(f)
+        print(" done!")
+    else:
+        print("Generating signature matrix...")      
+        random.seed(27945)
+        funcs = [gen_hash_func(len(all_words)) for i in xrange(num_hash_funcs)]
+        matrix = signature_matrix(funcs, docs)
+
+    # dump matrix
+    if args.matrix_output is not None:
+        sys.stdout.write("Saving matrix to file...")
+        sys.stdout.flush()
+        save_sig_matrix(matrix, args.matrix_output)
+        print(" done!\nSignature matrix written to {}".format(args.matrix_output))
+
+    doc_id_1 = int(args.document1)
+    doc_id_2 = int(args.document2)
+    jaccard_estimate = jarccard_probability(matrix, doc_id_1, doc_id_2, num_hash_funcs)
     actual_jaccard = compute_jaccard(doc_id_1, doc_id_2, docs)
     print "Estimated jaccard: {0}, actual jaccard: {1}".format(jaccard_estimate, actual_jaccard)
 
